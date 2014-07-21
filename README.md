5380515279caac275bf86572a8ce25c91db9f7f6
Ryan Jenkins <rjenkins@mail.sfsu.edu>
2014-07-21 03:53:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# An OSX IRC Client That Doesn't Look Like Shit: AOICTDLLS
+
+Seriously guys, colloquy looks like the devs haven't touched the UI in half a
+decade, textual costs money and isn't much better, limechat's window splitting
+freaks me the hell out, XChat is a passable port but the tabbing is lame and
+god only knows which features work and which don't, and finally if I wanted a
+curses client I wouldn't be paying the premium for OSX eye candy. The worst
+part is that on top the poor visuals most of these clients have broken DCC
+support. I figure if all you need to support is the most commonly used IRC
+features to be a major client I might as well make one that's easy on the eyes.
+Thus is born An OSX IRC Client That Doesn't Look Like Shit.
+
+## TODO
+
+- Not look like shit
---
+++ main.js
@@ -0,0 +1,124 @@
+var root;
+
+;(function() {
+  var irc = require('irc')
+
+  function ensureMethod(obj, attr) {
+    // Returns a function that, when called, arrangeds for obj.attr to be 
+    // called with the same args where `this` refers to obj
+    return (function() { return obj[attr].apply(obj, arguments) })
+  }
+
+  function WindowMVM() {
+    var self = this
+    self.networks = ko.observableArray()
+    self.input = ko.observable()
+    self.defaultNick = "mibiot"
+
+    // A "tab" can be a network, channel, or null when none of either are 
+    // available.
+    self.activeTab = ko.observable(self)
+  }
+  WindowMVM.prototype = {
+    connect: function(options) {
+      var network = new NetworkMVM(options)
+      this.networks.push(network)
+      this.activeTab(network)
+    },
+    lineSubmit: function(_, e) {
+      if (e.keyCode == 13) {
+        this.activeTab().send(this.input())
+        this.input('')
+      } else {
+        return true
+      }
+    },
+    send: function(line) {
+      var match = null
+
+      if (match = line.match(/^\/connect (\S+) ?(\d+)?/)) {
+        var opts = {
+          host: match[1],
+          port: parseInt(match[2] | '6667'),
+          nick: this.defaultNick
+        }
+
+        this.connect(opts)
+      } else if (match = line.match(/^\/nick (\S+)$/)) {
+        this.defaultNick = match[1]
+      }
+    }
+  }
+
+  function NetworkMVM(options) {
+    var self = this
+    self.channels = ko.observableArray()
+    self.lines = ko.observableArray()
+
+    var n = options.host.split('.')
+    while (n.length > 2) n.shift()
+    self.name = n.join('.')
+
+    // Connect to the network, we're never going to make a network and not
+    // connect to it.
+    self.client = new irc.Client(options.host, options.nick, options)
+    self.client.on('notice', ensureMethod(self, 'onNotice'))
+    self.client.on('motd', ensureMethod(self, 'onMotd'))
+    self.client.on('join', ensureMethod(self, 'onJoin'))
+    self.client.on('raw', function(e) { console.log(e) })
+  }
+  NetworkMVM.prototype = {
+    join: function(channel) {
+      this.client.join(channel)
+    },
+    send: function(line) {
+      var match = null
+
+      if (match = line.match(/^\/join (\S+)/)) {
+        this.join(match[1])
+      }
+    },
+    onNotice: function(nick, to, text, e) {
+      this.lines.push({
+        left: nick || '*',
+        right: text,
+        lineClass: 'notice'
+      })
+    },
+    onMotd: function(motd) {
+      this.lines.push({
+        left: '*',
+        right: motd,
+        lineClass: 'notice'
+      })
+    },
+    onJoin: function(channel, nick, message) {
+      console.log(message)
+    }
+  }
+
+  function ChannelMVM(client, options) {
+    var self = this
+    self.lines = ko.observableArray()
+    self.users = ko.observableArray()
+    self.name = ko.observable(options.channel)
+
+    self.channel = client.channel(options.channel)
+  }
+  ChannelMVM.prototype = {
+    onMessage: function(message) {
+      this.messages.push(message)
+      console.log(message)
+    },
+    formatForDisplay: function(message) {
+      return {
+        left: message.nickname,
+        right: message.text,
+        lineClass: 'message'
+      }
+    }
+  }
+
+  root = new WindowMVM()
+  ko.applyBindings(root)
+})()
